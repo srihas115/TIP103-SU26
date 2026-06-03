9e0ad45a8cd76dacf2932c024f512a194ee93059
srihas <srihasgupta@gmail.com>
2026-06-02 21:02:24 -0500

    Add linear search function and example usage for Problem 1: Hunny Hunt

---
+++ Unit 1/Session 1/Problem Set 1/Problem 1: Hunny Hunt.py
@@ -0,0 +1,16 @@
+def linear_search(items, target):
+	for i, item in enumerate(items):
+		if item == target:
+			print(i)
+			return
+	print(-1)
+
+			
+
+items = ['haycorn', 'haycorn', 'haycorn', 'hunny', 'haycorn']
+target = 'hunny'
+linear_search(items, target)
+
+items = ['bed', 'blue jacket', 'red shirt', 'hunny']
+target = 'red balloon'
+linear_search(items, target)
